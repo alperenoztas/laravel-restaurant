9c09e6510df064c0d0d70b18578247a2dfe79413
Alperen Öztaş <alprnoztas@gmail.com>
2022-09-12 17:41:58 +0300

    category menu relationship

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 class Category extends Model
 {
     use HasFactory;
@@ -14,4 +14,14 @@ class Category extends Model
         'image',
         'description',
     ];
+
+    /**
+     * The menus that belong to the Category
+     *
+     * @return BelongsToMany
+     */
+    public function menus(): BelongsToMany
+    {
+        return $this->belongsToMany(Menu::class,'category_menu');
+    }
 }
--- app/Models/Menu.php
+++ app/Models/Menu.php
@@ -2,10 +2,21 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Menu extends Model
 {
     use HasFactory;
+
+    /**
+     * The categories that belong to the Menu
+     *
+     * @return BelongsToMany
+     */
+    public function categories(): BelongsToMany
+    {
+        return $this->belongsToMany(Category::class, 'category_menu');
+    }
 }
